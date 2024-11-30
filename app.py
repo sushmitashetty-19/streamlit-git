f9dbb4e422a4b122f65bb627a0c142425362bebb
Sushmita Shetty <sushmita.shetty0319@gmail.com>
2024-11-30 11:09:17 +0530

    Initial commit: Added .gitignore, app.py, and requirements.txt

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Streamlit demo')
+name =st.text_input('Enter your name: ')
+st.write("you entered:" , name)
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+df = pd.DataFrame({'Name': ['abc', 'xyz', 'pqr'], 'City': ['mumbai', 'delhi', 'pune']})
+st.table(df)
